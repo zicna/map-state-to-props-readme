318a5cea5ecf9128b266220de32843b9b724204b
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-11-19 09:45:44 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import counterReducer from './reducers/counterReducer.js';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import counterReducer from "./reducers/counterReducer.js";
+import App from "./App";
+import "./index.css";
 
 const store = createStore(
-    counterReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-); 
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>,
+  document.getElementById("root")
+);
